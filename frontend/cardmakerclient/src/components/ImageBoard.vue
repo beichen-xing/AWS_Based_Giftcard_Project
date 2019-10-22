93263a082e6f791e7902a626435d31381241be4a
henry <henry.ik@hotmail.com>
2019-10-22 17:34:02 -0400

    Starting Editor Board.

---
+++ frontend/cardmakerclient/src/components/ImageBoard.vue
@@ -0,0 +1,7 @@
+<template>
+  <div>
+    <img alt="Vue logo" src="../assets/test.jpg" height="200" width="300" />
+  </div>
+</template>
+
+
--- frontend/cardmakerclient/src/views/Editor.vue
+++ frontend/cardmakerclient/src/views/Editor.vue
@@ -5,15 +5,20 @@
       <Menu />
     </div>
     <div>this is the board</div>
+    <div>
+      <EditorBoard />
+    </div>
   </div>
 </template>
 
 <script>
 import Menu from "../components/EditorMenu";
+import EditorBoard from "../components/EditorBoard";
 export default {
   name: "Editor",
   components: {
-    Menu
+    Menu,
+    EditorBoard
   }
 };
 </script>
