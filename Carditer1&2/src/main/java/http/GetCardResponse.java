476cb3afc66622317c2195946438d733265b203d
beichen-xing <53940972+beichen-xing@users.noreply.github.com>
2019-11-16 16:52:10 -0500

    iter2

---
+++ Carditer1&2/src/main/java/http/GetCardResponse.java
@@ -0,0 +1,37 @@
+package http;
+
+
+/*===========Response with status code==================*/
+
+public class GetCardResponse {
+	public final String id;
+	public final int statusCode;
+	public final String type;
+	public final String name;
+	public final String recipient;
+	public final String orientation;
+	public final String textelement;
+	
+
+	public GetCardResponse(int Code, String id,String name,String recipient, String type,String orientation, String textelement) {
+		this.statusCode = Code;
+		this.id = id;
+		this.name = name;
+		this.recipient = recipient;
+		this.type = type;
+		this.orientation = orientation;
+		this.textelement = textelement;
+	}
+
+
+	public GetCardResponse(int code) {
+		this.statusCode = code;
+		this.id = "";
+		this.name = "";
+		this.recipient = "";
+		this.type = "";
+		this.orientation = "";
+		this.textelement = "";
+		
+	}
+}
